9e7f4aa3622a4fc3d740d2912805845cd7536db1
artem hloba <artemgloba2@gmail.com>
2024-09-13 22:03:49 +0300

    Fix webpack.config

--- lab_5/webpack.config.js
+++ lab_5/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
@@ -11,34 +10,40 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: `[name].bundle.js`
+    filename: '[name].[contenthash].bundle.js',
+    clean: true
   },
   plugins: [
-    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Index',
       template: path.resolve(__dirname, './src/pages/index.html'),
-      filename: 'index.html'
+      filename: 'index.html',
+      chunks: ['main']
     }),
     new HtmlWebpackPlugin({
       title: 'Photo',
       template: path.resolve(__dirname, './src/pages/photo.html'),
-      filename: 'photo.html'
+      filename: 'photo.html',
+      chunks: ['main']
     }),
     new HtmlWebpackPlugin({
-      title: 'rozklad',
+      title: 'Rozklad',
       template: path.resolve(__dirname, './src/pages/rozklad.html'),
-      filename: 'rozklad.html'
+      filename: 'rozklad.html',
+      chunks: ['main']
     }),
     new HtmlWebpackPlugin({
       title: 'News',
       template: path.resolve(__dirname, './src/pages/news.html'),
-      filename: 'news.html'
+      filename: 'news.html',
+      chunks: ['main']
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
     }),
     new CopyPlugin({
       patterns: [{ from: './src/assets', to: 'assets' }]
-    }),
-    new MiniCssExtractPlugin()
+    })
   ],
   module: {
     rules: [
@@ -49,10 +54,31 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[hash][ext][query]'
+        }
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[hash][ext][query]'
+        }
       }
     ]
   },
   devServer: {
-    port: 8080
+    port: 8080,
+    open: true,
+    hot: true,
+    liveReload: true,
+    static: {
+      directory: path.resolve(__dirname, './dist')
+    },
+    historyApiFallback: true
   }
 }
